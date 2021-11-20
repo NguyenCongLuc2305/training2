9138917af417ec8c2b2e7075ef81567ebc935e22
someone <someone@someplace.com>
2021-11-20 17:37:09 +0700

    some init msg

---
+++ resources/views/templates/master.blade.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title>@yield('title')</title>
+</head>
+<body>
+@section('sidebar')
+    This is the master sidebar.
+@show
+<div class="container">
+    @yield('content')
+        <div class="container">
+            @if (Auth::check())
+                <div>
+                    Bạn đang đăng nhập với quyền
+                    @if( Auth::user()->level == 1)
+                        {{ "SuperAdmin" }}
+                    @elseif( Auth::user()->level == 2)
+                        {{ "Admin" }}
+                    @elseif( Auth::user()->level == 3)
+                        {{ "Thành viên" }}
+                    @endif
+                </div>
+                <div class="pull-right" style="margin-top: 3px;"><a class="btn btn-primary" href="/{{ url('/logout') }}">Đăng
+                        xuất</a></div>
+            @endif
+        </div>
+</div>
+</body>
+</html>
+
+
